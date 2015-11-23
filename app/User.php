a87fe23a2b766b0844656b119fdbf3ca134b6ea4
long nguyen <longn@askhanuman.com>
2015-11-23 10:14:31 +0700

    Update User.php
    fix indent

--- app/User.php
+++ app/User.php
@@ -24,6 +24,6 @@ class User extends Model implements UserContract, RemindableContract {
 	 */
 	protected $hidden = ['password', 'remember_token'];
 
-    protected $fillable = ['username', 'email', 'avatar'];
+        protected $fillable = ['username', 'email', 'avatar'];
 
 }
